0ca67cccd50d83da5afddb324fc91993ce15ab94
Gustavo Marques <gmartes23@gmail.com>
2026-04-10 09:53:42 -0300

    remove ssl config

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import asyncio
 import logging
 import os
-import ssl
-import aiohttp
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.client.session.aiohttp import AiohttpSession
@@ -15,16 +13,8 @@ TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 logging.basicConfig(level=logging.INFO)
 
-async def main():
-    ssl_context = ssl.create_default_context()
-    ssl_context.check_hostname = False
-    ssl_context.verify_mode = ssl.CERT_NONE
-    
-    connector = aiohttp.TCPConnector(ssl=ssl_context)
-    session = AiohttpSession()
-    session._connector = connector
-    
-    bot = Bot(token=TOKEN, session=session)
+async def main():    
+    bot = Bot(token=TOKEN)
     dp = Dispatcher()
     dp.include_router(start_router)
     dp.include_router(analyze_router)
